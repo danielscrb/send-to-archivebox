4fdd56167723dfbe1b8e375efee10798a43260bd
Daniel <git@casasc.org>
2025-02-09 20:43:48 +0100

    added /add command

---
+++ src/main.py
@@ -0,0 +1,59 @@
+import telebot
+from pyarchivebox import PyArchiveBox
+import config
+
+BOT_TOKEN = config.telegram_token
+bot = telebot.TeleBot(BOT_TOKEN)
+
+archivebox = PyArchiveBox(config.archivebox_username, config.archivebox_password, config.archivebox_url)
+archivebox.login()
+
+whitelist = config.whitelist
+def is_allowed(userid):
+    return userid in whitelist
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    if is_allowed(message.from_user.id):
+        bot.reply_to(message, "You are allowed to use this bot.")
+    else:
+        bot.reply_to(message, "You are not allowed to use this bot, all your commands will not execute.")
+
+
+@bot.message_handler(commands=["add"])
+def add_url(message):
+    if not is_allowed(message.from_user.id):
+        bot.reply_to(message, "You are not allowed to use this bot.")
+        return
+    
+    command_parts = message.text.split(maxsplit=1)
+    if len(command_parts) < 2:
+        bot.reply_to(message, "Usage: /add <url> [tags] [depth].")
+        return
+    
+    args = command_parts[1].split(maxsplit=2)
+    url = args[0]
+    tags = args[1] if len(args) > 1 else None
+    depth = args[2] if len(args) > 2 else None
+
+    bot.send_message(message.chat.id, "Arching started...")
+    if tags and depth:
+        archivebox.add(url, tags, depth=depth)
+    elif tags:
+        archivebox.add(url, tags)
+    else:
+        archivebox.add(url)
+
+    latest = archivebox.get_latest()
+    last = latest.get(0)
+
+    response = f"Archived url: {url}\nArchive title: {last['title']}\nArchive date added: {last['date_added']}\nArchive size: {last['size']}"
+    if tags:
+        response += f"Archive tags: {tags}\n"
+    if depth:
+        response += f"Archive depth: {depth}\n"
+
+    bot.reply_to(message, response)
+
+
+bot.infinity_polling()
