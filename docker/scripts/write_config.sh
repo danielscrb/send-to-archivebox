001ced0247c09c3a7eb53f736b2a9be23f7e74e5
Daniel <git@casasc.org>
2025-02-16 09:32:08 +0100

    updated write_config script

--- docker/scripts/write_config.sh
+++ docker/scripts/write_config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-echo "telegram_token=${TG_TOKEN}" > /data/send-to-archivebox/src/config.py
+touch /data/send-to-archivebox/src/config.py
+echo "telegram_token='${TG_TOKEN}'" >> /data/send-to-archivebox/src/config.py
 echo "whitelist={${WHITELIST}}" >> /data/send-to-archivebox/src/config.py
-echo "archivebox_username=${ARCHIVEBOX_USER}" >> /data/send-to-archivebox/src/config.py
-echo "archivebox_password=${ARCHIVEBOX_PASS}" >> /data/send-to-archivebox/src/config.py
-echo "archivebox_url=${ARCHIVEBOX_URL}" >> /data/send-to-archivebox/src/config.py
+echo "archivebox_username='${ARCHIVEBOX_USER}'" >> /data/send-to-archivebox/src/config.py
+echo "archivebox_password='${ARCHIVEBOX_PASS}'" >> /data/send-to-archivebox/src/config.py
+echo "archivebox_url='${ARCHIVEBOX_URL}'" >> /data/send-to-archivebox/src/config.py
