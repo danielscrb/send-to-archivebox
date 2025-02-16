743a7d1df45cd0627a7f0e9807585cc8d75a74ab
Daniel <git@casasc.org>
2025-02-16 10:13:18 +0100

    updated write_config to check if file already exists

--- docker/scripts/write_config.sh
+++ docker/scripts/write_config.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-touch /data/send-to-archivebox/src/config.py
-echo "telegram_token='${TG_TOKEN}'" >> /data/send-to-archivebox/src/config.py
-echo "whitelist={${WHITELIST}}" >> /data/send-to-archivebox/src/config.py
-echo "archivebox_username='${ARCHIVEBOX_USER}'" >> /data/send-to-archivebox/src/config.py
-echo "archivebox_password='${ARCHIVEBOX_PASS}'" >> /data/send-to-archivebox/src/config.py
-echo "archivebox_url='${ARCHIVEBOX_URL}'" >> /data/send-to-archivebox/src/config.py
+CONFIG_FILE="/data/send-to-archivebox/src/config.py"
+
+if [ ! -f "$CONFIG_FILE" ]; then
+  echo "Generating config file"
+  cat <<EOF > "$CONFIG_FILE"
+telegram_token='${TG_TOKEN}'
+whitelist={${WHITELIST}}
+archivebox_username='${ARCHIVEBOX_USER}'
+archivebox_password='${ARCHIVEBOX_PASS}'
+archivebox_url='${ARCHIVEBOX_URL}'
+EOF
+else
+  echo "Config file already exists, skipping generation."
+fi
+
+exec "$@"
